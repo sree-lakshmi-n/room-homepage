6273842a418f6d169c5b4bae233cf3ba233017b6
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-20 08:43:32 +0530

    Added white background to navbar on click

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 `use strict`;
+const hamburger = document.getElementsByClassName("hamburger-icon")[0];
+const nav = document.getElementsByClassName("top-nav")[0];
+hamburger.addEventListener("click", (element) => {
+  console.log(element.target.parentElement);
+  element.target.parentElement.classList.toggle("open");
+  document.body.classList.toggle("fixed-position");
+  nav.classList.toggle("white-background");
+});
