edd30f30b6fcefebf821b553a84815c27750b17a
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-20 10:52:57 +0530

    Added hide and position-and-colour classes to corresponding elements on toggle

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 `use strict`;
 const hamburger = document.getElementsByClassName("hamburger-icon")[0];
 const nav = document.getElementsByClassName("top-nav")[0];
+const logo = document.getElementsByClassName("logo")[0];
+const menu = document.getElementsByClassName("menu")[0];
+
 hamburger.addEventListener("click", (element) => {
-  console.log(element.target.parentElement);
   element.target.parentElement.classList.toggle("open");
-  document.body.classList.toggle("fixed-position");
+  document.body.classList.toggle("position-and-colour");
   nav.classList.toggle("white-background");
+  logo.classList.toggle("hide");
+  menu.classList.toggle("hide");
 });
