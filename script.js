ee0683bb2cc3402fb3bd4c5679196b05a0333d55
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-20 12:12:36 +0530

    Fetched overlay and wrapper element. Toggled avoid-clicks and hide classes for wrapper and overlay respectively

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const hamburger = document.getElementsByClassName("hamburger-icon")[0];
 const nav = document.getElementsByClassName("top-nav")[0];
 const logo = document.getElementsByClassName("logo")[0];
 const menu = document.getElementsByClassName("menu")[0];
+const overlay = document.getElementsByClassName("overlay")[0];
+const main = document.getElementsByClassName("wrapper")[0];
 
 hamburger.addEventListener("click", (element) => {
   element.target.parentElement.classList.toggle("open");
-  //   document.body.classList.toggle("position-and-colour");
+  main.classList.toggle("avoid-clicks");
   nav.classList.toggle("white-background");
   logo.classList.toggle("hide");
   menu.classList.toggle("hide");
+  overlay.classList.toggle("hide");
 });
-
-// window.addEventListener("click", (element) => {
-//   console.log(element.target.parentElement);
-// });
